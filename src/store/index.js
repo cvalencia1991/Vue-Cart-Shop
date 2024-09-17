6ffacc10a2c3b2c89e501aced0ec8d015c4133d2
cvalencia1991 <cesar4a6z@gmail.com>
2024-09-17 11:08:04 -0500

    Implement Index Store for vuex

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import { createStore } from 'vuex';
+
+import productModule from './modules/products.js'
+import cartModule from './modules/cart.js'
+
+const store = createStore({
+  modules: {
+    prods: productModule,
+    cart: cartModule
+  },
+})
+
+export default store;
